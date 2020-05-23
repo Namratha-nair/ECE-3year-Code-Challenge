155e23c0c5665c3fd0fddf7707822342aaf9c16a
Rashmitha-22 <65492902+Rashmitha-22@users.noreply.github.com>
2020-05-23 14:36:34 +0530

    Add files via upload

---
+++ RASHMITHA_4AL17EC077.py
@@ -0,0 +1,15 @@
+print('Enter correct username and password  to continue')
+count=0
+while count < 3:
+    username = input('Enter username: ')
+    password = input('Enter password: ')
+    if password=='e3$WT89x' and username=='Micheal':
+        print('login successful')
+        
+    else:
+         count += 1
+         print('Incorrect username/password.Try again')
+         
+        
+    if count==3:
+        print('Account locked.' )
